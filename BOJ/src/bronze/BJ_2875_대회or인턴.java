7b00cf5f3c8dd6f04bdde6e7e7ce4e1bcb715401
yogurt05j <yogurt05j@gmail.com>
2023-03-25 17:57:46 +0900

    0325

--- BOJ/src/bronze/BJ_2875_대회or인턴.java
+++ BOJ/src/bronze/BJ_2875_대회or인턴.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BJ_2875_대회or인턴 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
