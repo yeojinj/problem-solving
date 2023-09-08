33f9970ec02fcdcc2ec8d3c39d28cdfd5e755aa1
Yeojin Jeong <63780092+yeojinj@users.noreply.github.com>
2023-09-08 23:23:53 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());       // 여학생의 수
+        int M = Integer.parseInt(st.nextToken());       // 남학생의 수
+        int K = Integer.parseInt(st.nextToken());       // 인턴쉽에 참여해야하는 인원
+
+        int team = 0;      // 팀 수
+
+        while (true) {
+            N -= 2;
+            M -= 1;
+            if (N + M >= K && N >= 0 && M >= 0) {
+                team++;
+                continue;
+            }
+            else {
+                break;
+            }
+        }
+
+        System.out.println(team);
+    }
+
+}
