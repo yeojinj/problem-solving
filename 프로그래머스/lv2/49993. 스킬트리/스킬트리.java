aeb8eae3afe126f416a807967b8f9a965b787967
Yeojin Jeong <63780092+yeojinj@users.noreply.github.com>
2023-10-07 00:08:27 +0900

    [level 2] Title: 스킬트리, Time: 0.06 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -4,24 +4,18 @@ class Solution {
         
         for (int i = 0; i < skill_trees.length; i++) {
             String skillTree = skill_trees[i];  // 스킬트리 하나씩 탐색
-            int skillIdx = 0;   // 몇 번째 스킬을 먼저 써야하는지
+            int skillIdx = 0;           // 현재 써야할 선행 스킬 인덱스 번호
             boolean isValid = true;    // 현재 스킬트리가 가능한 건지 여부
             
             for (int j = 0; j < skillTree.length(); j++) {  // 현재 스킬트리 탐색
-                char s = skillTree.charAt(j);
-                for (int k = skillIdx; k < skill.length(); k++) {  // 선행 스킬 순서 탐색
-                    if (skill.charAt(k) == s) {     // 선행 스킬 순서에 있는 스킬 사용할 차례임
-                        if (k == skillIdx) {        // 선행 스킬 순서에 맞게 썼을 경우
-                            skillIdx++;
-                        } else {                    // 선행 스킬 순서에 안 맞게 썼을 경우
-                            isValid = false;
-                            break;
-                        }
+                int k = skill.indexOf(skillTree.substring(j, j + 1));
+                if (k != -1) {
+                    if (k == skillIdx) {        // 선행 스킬 순서에 맞게 썼을 경우
+                        skillIdx++;
+                    } else {                    // 선행 스킬 순서에 안 맞게 썼을 경우
+                        isValid = false;
+                        break;
                     }
-                }
-                
-                if (!isValid) {
-                    break;
                 }
             }
             
