88091a7dcba3215bfe916e1ebf29527df7e191fd
Yeojin Jeong <63780092+yeojinj@users.noreply.github.com>
2024-01-04 00:03:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 148 ms, Memory: 14632 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -6,9 +6,6 @@ import java.util.StringTokenizer;
 class Gem {
     int w;  // 무게
     int v;  // 가치
-
-    public Gem() {
-    }
 }
 
 public class Main {
@@ -28,20 +25,14 @@ public class Main {
             gems[i].v = Integer.parseInt(st.nextToken());   // i번 물건의 가치
         }
 
-        int[][] dp = new int[N + 1][K + 1];        // (1 ~ N)
-
-        for (int k = 1; k <= K; k++) {// for k 무게
-            for (int i = 1; i <= N; i++) {// for i 물건 번호
-                dp[i][k] = dp[i - 1][k];
-                // if 물건 넣을 수 있으면 (-> k - gems[i].w >= 0)
-                if (k - gems[i].w >= 0) {
-                    dp[i][k] = Math.max(dp[i][k], gems[i].v + dp[i - 1][k - gems[i].w]);    // i번 물건을 넣음
-                }
+        int[] dpdp = new int[K + 1];                // O(NK) 이하, 메모리 절약
+        for (int i = 1; i <= N; i++) {
+            for (int k = K; k >= gems[i].w; k--) {
+                dpdp[k] = Math.max(dpdp[k], dpdp[k - gems[i].w] + gems[i].v);
             }
         }
 
-        // 답 N K
-        System.out.println(dp[N][K]);
+        System.out.println(dpdp[K]);
 
     }
 }
